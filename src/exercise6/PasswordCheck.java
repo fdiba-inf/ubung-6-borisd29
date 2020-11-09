91afa69636e72ca6ffce231467ca15c4ebebba3e
borisd29 <foster.mark39@yahoo.com>
2020-11-09 16:18:26 +0000

    sd

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -4,6 +4,34 @@ import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        boolean noSymbols = true;
+        int digitCounter = 0;
+        String password;
+        System.out.print("Enter Password: ");
+
+        while(noSymbols && digitCounter < 2){
+            if(input.hasNext()) {
+                password = input.next();
+            }else{
+                System.out.println("Shit");
+                break;
+            }
+            if (password.length() <= 7) {
+                continue;
+            }
+
+            for (int i = 0; i < password.length(); i++) {
+                if(!Character.isLetterOrDigit(password.charAt(i))){
+                    noSymbols = false;
+                }
+                if(Character.isDigit(password.charAt(i))){
+                    digitCounter++;
+                }
+            }
+        }
+        System.out.println("Password valid!");
     }
 }
